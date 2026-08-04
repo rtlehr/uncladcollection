876eb762c92d22eee918182c8ef329a69a08c2d4
Ross Lehr <itsme@rosslehr.com>
2026-08-04 13:01:21 -0400

    Fixed seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,19 +3,18 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (app()->environment(['local', 'testing'])) {
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ]);
+        }
 
         $this->call([
             SiteSettingSeeder::class,
@@ -25,14 +24,17 @@ class DatabaseSeeder extends Seeder
             PageHelpSeeder::class,
             PublicPageSeeder::class,
             DevelopmentUserSeeder::class,
+
             CategorySeeder::class,
             TagSeeder::class,
-            AiKeywordExclusionSeeder::class,
-            AiContentStudioSeeder::class,
             CollectionSeeder::class,
             LicenseTypeSeeder::class,
             AssetConfigurationTemplateSeeder::class,
             SupportTicketCategorySeeder::class,
+
+            AiKeywordExclusionSeeder::class,
+            AiProviderSeeder::class,
+            AiContentStudioSeeder::class,
         ]);
 
         if (app()->environment(['local', 'testing'])) {
